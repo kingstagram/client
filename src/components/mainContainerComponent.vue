162858b6bb482dc49dd364c88069548bbf630705
michaelwp <michael.wenceslaus@gmail.com>
2019-12-06 15:26:27 +0700

    place-in-column-card

--- src/components/mainContainerComponent.vue
+++ src/components/mainContainerComponent.vue
@@ -4,16 +4,17 @@
         <div class="ui segment">
             <add-posting-component></add-posting-component>
         </div>
-        <div class="ui segment">
-            <p>
+        <div class="ui four column grid">
+            <div class="row">
                 <card-component v-for="post in posts"
                                 :key="post._id"
                                 :post="post"
                                 @like="getPosts"
                                 @add-comment="getPosts"
-                ></card-component>
-            </p>
-            <user-profile></user-profile>
+                                class="column">
+                </card-component>
+            </div>
+            <!--&lt;!&ndash;            <user-profile></user-profile>&ndash;&gt;-->
         </div>
     </div>
 </template>
@@ -23,42 +24,56 @@
     import mainNavbarComponent from "./mainNavbarComponent";
     import cardComponent from "./cardComponent";
     import addPostingComponent from "./addPostingComponent";
-    import userProfile from "./userProfile";
-    
+    // import userProfile from "./userProfile";
+    const url = "http://104.198.195.12";
+
     export default {
         name: "mainContainerComponent",
-                data () {
+        data() {
             return {
                 posts: [],
                 message: '',
                 isMessage: false,
+                author: null
             }
         },
         components: {
             mainNavbarComponent,
             cardComponent,
-            addPostingComponent
-            userProfile
+            addPostingComponent,
+            // userProfile
         },
         methods: {
-            getPosts: function(){
-                alert('masuk sini')
+            getPosts: function () {
                 axios({
-                    // method: 'get',
-                    url: 'http://localhost:3000/posts/all',
+                    method: 'get',
+                    url: url + '/posts/all',
                     headers: {
-                        // token: localStorage.getItem('token')
-                        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZGU5ZDFkOTYyZjhkNzMzOGUxYzE5ZTIiLCJ1c2VybmFtZSI6ImFhYSIsImVtYWlsIjoiYmJiQG1haWwuY29tIiwiaWF0IjoxNTc1NjA0Njk3fQ.VG8s4hNlu6fg_zGD7qwGOeFCKre3ZZO15J-KFr0sGLM"
+                        token: localStorage.token
                     }
                 }).then(response => {
-                    console.log(response.data)
+                    console.log(response.data);
+                    this.posts = response.data
+                }).catch(err => {
+                    this.message = err;
+                    this.isMessage = true;
+                });
+            },
+            getAuthor: function (userId) {
+                axios({
+                    method: 'get',
+                    url: url + '/users/',
+                    headers: {
+                        token: localStorage.token
+                    }
+                }).then(response => {
+                    console.log(response.data);
                     this.posts = response.data
                 }).catch(err => {
                     this.message = err;
                     this.isMessage = true;
                 });
             }
-
         },
         created() {
             this.getPosts()
@@ -67,5 +82,7 @@
 </script>
 
 <style scoped>
-
+    .column{
+        margin: 5px;
+    }
 </style>
